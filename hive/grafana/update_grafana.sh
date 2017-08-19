c06948d50276f6fbcecd415864f1ede272ad6e51
ORFG <onerealfunnyguy@gmail.com>
2017-08-19 12:00:58 -0700

    add glances mv stuff

--- hive/grafana/update_grafana.sh
+++
@@ -1,6 +0,0 @@
-docker run -d \
-    --name="grafana" \
-    -p 3000:3000 \
-    -v /home/$USER/grafana/db/:/var/lib/grafana/ \
-    -v /home/$USER/grafana/config/:/etc/grafana/ \
-     grafana/grafana
--- hq/zoneminder/update_zm.sh
+++ hq/zoneminder/update_zm.sh
@@ -1,5 +1,6 @@
 docker run -d \
 --name="Zoneminder-1.29" \
+--restart always \
 --privileged=true \
 -v /home/$USER/zoneminder/config:/config:rw \
 -v /etc/localtime:/etc/localtime:ro \
